2a98b4e82b5e0678e4fcb32b8dfa65152edd010d
Bread-ou <93944942+Bread-ou@users.noreply.github.com>
2023-02-13 10:23:35 +0000

    Upload images crude method created
    added a method to upload an image to the images file

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 // Requirments to run the sever.
 const express = require('express')
 const app = express()
+const multer = require('multer')
+const path = require ('path')
+
+// Image storage method.
+const storage = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+        console.log(file)
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+})
+const upload = multer({storage: storage})
 
 // Setting up the view engine.
 app.set('view engine', 'ejs')
-app.set('views', _dirname + '/views')
+app.set('views', __dirname + '/views')
 
 
-app.get('/', (req, res)=> {
+app.get('/upload', (req, res)=> {
     res.render('index')
+})
+
+app.post('/upload', upload.single('image'), (req,res)=>{
+    res.send("Image Uploaded")
 })
 
 app.listen(3000)
