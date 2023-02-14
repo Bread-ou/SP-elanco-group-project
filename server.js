e7371befd219623e6b0912758f988beda23c5a06
Bread-ou <93944942+Bread-ou@users.noreply.github.com>
2023-02-14 20:40:51 +0000

    Method to send and fetch photos from API, as well as display them with the labels after.

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const multer = require('multer')
 const path = require ('path')
 const PORT = process.env.PORT || 3000
 
+// Google cloud vision setup.
+const vision = require('@google-cloud/vision')
+const client = new vision.ImageAnnotatorClient({
+    keyFilename: './Key.json'
+  })
+
 // Image storage method.
 const storage = multer.diskStorage({
     destination: (req, file, cb) =>{
@@ -20,14 +26,20 @@ const upload = multer({storage: storage})
 // Setting up the view engine.
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
+app.use('/images', express.static('images'))
 
 
 app.get('/upload', (req, res)=> {
     res.render('index')
 })
 
-app.post('/upload', upload.single('image'), (req,res)=>{
-    res.send("Image Uploaded")
+app.post('/upload', upload.single('image'), async (req,res)=>{
+    const [result] = await client.labelDetection(req.file.path)
+    const labels = result.labelAnnotations.map(label => {
+    return { description: label.description, score: label.score }
+  })
+  const imageUrl = '/images/' + path.basename(req.file.path)
+  res.render('labels', { labels, imageUrl });
 })
 
 app.listen(PORT, ()=>{
