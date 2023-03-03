548610af1f22cbdacc6f24813b6c23bb7c69a4fc
Bread-ou <93944942+Bread-ou@users.noreply.github.com>
2023-03-03 20:56:39 +0000

    file handling fixes

--- routes/upload.js
+++ routes/upload.js
@@ -29,14 +29,14 @@ const storage = multer.diskStorage({
 const upload = multer({
     storage: storage,
     limits: {
-        fileSize: 4000000
-        },
-        fileFilter(req, file, cb) {
+        fileSize: 5000000
+    },
+    fileFilter(req, file, cb) {
         if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-        return cb( new Error('Please upload a valid image file'))
+        return cb(('Please upload a valid image file!'))
         }
         cb(undefined, true)
-        }
+    }
 
 })
 
@@ -47,28 +47,37 @@ router.get('/', (req, res)=> {
 
 // Post method used to upload image to the server, send and receive labels from the API, and forward that to the front end.
 router.post('/', upload.single('image'), async (req,res)=>{
-    // Send image and return the results from the API.
-    const [result] = await client.labelDetection(req.file.path)
-    const labels = result.labelAnnotations.map(label => {
-    return { description: label.description, score: label.score }
-  })
-  // Sort the labels in order of highest confidence.
-  
-  // TODO: Check if it does that alredy.
-  let sortedLabels = labels.sort((a, b) => b.score - a.score)
-  const imageUrl = '/images/' + path.basename(req.file.path)
 
-  // TODO: Write a function to iterate through labels map and compare and remove unnecessary labels.
-  const newLabels = labelChecker.filterLabels(sortedLabels)
-  sortedLabels = labelChecker.seperateLabels(newLabels, sortedLabels)
+    // If the file matches the specifications made.
+    if (req.file) {    
+        // Send image and return the results from the API.
+        const [result] = await client.labelDetection(req.file.path)
+        const labels = result.labelAnnotations.map(label => {
+        return { description: label.description, score: label.score }
+        })
+        // Sort the labels in order of highest confidence.
+        
+        // TODO: Check if it does that alredy.
+        let sortedLabels = labels.sort((a, b) => b.score - a.score)
+        const imageUrl = '/images/' + path.basename(req.file.path)
 
-  // Save both types of labels in seperate json files. 
-  fileSave.saveLabels(sortedLabels, imageUrl, 'unfilteredLabels.json')
-  fileSave.saveLabels(newLabels, imageUrl, 'filteredLabels.json')
+        // TODO: Write a function to iterate through labels map and compare and remove unnecessary labels.
+        const newLabels = labelChecker.filterLabels(sortedLabels)
+        sortedLabels = labelChecker.seperateLabels(newLabels, sortedLabels)
+
+        // Save both types of labels in seperate json files. 
+        fileSave.saveLabels(sortedLabels, imageUrl, 'unfilteredLabels.json')
+        fileSave.saveLabels(newLabels, imageUrl, 'filteredLabels.json')
 
 
-  // Render labels ejs file and pass on the sorted labels and image.
-  res.render('labels', {newLabels, sortedLabels, imageUrl });
+        // Render labels ejs file and pass on the sorted labels and image.
+        res.render('labels', {newLabels, sortedLabels, imageUrl })
+// Add proper error handling when the image is too large.
+} else {
+    // Error handling when not image, send responce.
+    res.status(400).send("Please upload a valid image")
+    //res.render('index')
+    }
 })
 
 module.exports = router
